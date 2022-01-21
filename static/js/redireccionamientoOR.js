b6869893fcfd26d697360cca53fe8ab1f390c067
figandgrape <maxzanuttini@figandgrape.io>
2022-01-21 01:59:12 -0300

    [FX Se agregan varios cambios, iframe y  footers]

--- static/js/redireccionamientoOR.js
+++
@@ -1,6 +0,0 @@
-function redireccionarOR() {
-    window.locationf = "https://widget.onramper.com/?color=1d2d50&apiKey=pk_test_3HMt2an1_Q5in3h4XT0bsnj8SqYizg3qgTQYH74vxwk0";
-  }
-  setTimeout("redireccionarOR()", 5000); //tiempo expresado en milisegundos
-
- 
